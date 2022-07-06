f6a10315cb5fd08862ab04b4b77c0d0e6db3aeb0
yongseongJang <kgucs13@gmail.com>
2022-07-06 22:09:00 +0900

    build: set up development environment

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJsPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { sourceMap: false } },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/public/index.html',
+      filename: './index.html',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserJsPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  devServer: {
+    host: '127.0.0.1',
+    port: 9001,
+    open: true,
+    hot: true,
+  },
+};
